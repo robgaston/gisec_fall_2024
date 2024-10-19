099baf3d7270691d944834e0c755c55a46c9ee53
Rob Gaston <rgaston@fargeo.com>
2024-10-19 19:42:40 +0000

    day 2 workshop work

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from "vue";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import style from "./mapbox/style.json";
+import neighborhoods from "./data/neighborhoods-with-count.json";
 
 mapboxgl.accessToken = "pk.eyJ1Ijoicmdhc3RvbiIsImEiOiJJYTdoRWNJIn0.MN6DrT07IEKXadCU8xpUMg";
 
@@ -12,9 +13,24 @@ onMounted(() => {
   const map = new mapboxgl.Map({
     "container": mapContainer.value,
     "style": style,
+    "bounds": [
+      -118.65226,
+      33.70555,
+      -118.1557,
+      34.33305
+    ],
+    "fitBoundsOptions": {
+      "padding": 25,
+      "pitch": 45
+    }
   });
 
   map.addControl(new mapboxgl.NavigationControl());
+
+  map.on("load", () => {
+    const neighborhoodsSource = map.getSource("neighborhoods");
+    neighborhoodsSource.setData(neighborhoods);
+  });
 });
 </script>
 
